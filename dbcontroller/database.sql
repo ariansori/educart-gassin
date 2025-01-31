73bb82996ca8a71b95014ef26dd6284e24166a43
Khoirul Ansori <khoirulansori407@gmail.com>
2025-01-31 13:01:23 +0700

    update syntax sql

--- dbcontroller/database.sql
+++ dbcontroller/database.sql
@@ -1,63 +1,230 @@
-CREATE TABLE Customers(
-CustomerID VARCHAR(14) NOT NULL PRIMARY KEY,
-FirstName VARCHAR(30),
-MiddleName VARCHAR(10),
-LastName VARCHAR(30),
-Birthdate DATE
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 09, 2024 at 06:19 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
-);
-
-CREATE TABLE CustomerAddresses(
-CustomerID VARCHAR(14) NOT NULL PRIMARY KEY,
-FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID),
-StreetAddress VARCHAR(30),
-City VARCHAR(30),
-HomeState VARCHAR(30),
-Country VARCHAR(30),
-ZipCode VARCHAR(12)
-);
-
-CREATE TABLE CustomerAccounts(
-CustomerID VARCHAR(14) NOT NULL PRIMARY KEY,
-FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID),
-Email VARCHAR(50),
-UserName VARCHAR(20),
-Password VARCHAR(8)
-);
-
-CREATE TABLE Products (
-ItemID INT(12) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-Code VARCHAR(20) NOT NULL,
-Name VARCHAR(30),
-Image text NOT NULL,
-Price FLOAT
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
-CREATE TABLE Orders(
-CustomerID VARCHAR(14) NOT NULL,
-FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID),
-OrderID VARCHAR(14) NOT NULL PRIMARY KEY,
-OrderStatus VARCHAR(14),
-OrderDate DATE
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-CREATE TABLE ProductOrders(
-OrderID VARCHAR(14) NOT NULL,
-FOREIGN KEY (OrderID) REFERENCES Orders(OrderID),
-Code VARCHAR(20) NOT NULL,
-ItemID  INT(12),
-Quanity INT
-);
+--
+-- Database: `tokoatk`
+--
 
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `admin`
+--
 
-INSERT INTO Products (Code, Name, Image, Price) VALUES
-('FLS','Flamingo Shirt', 'img/products/flamingoShirt.jpg', 35.99),
-('GJ','Geisha Jacket', 'img/products/skullgeishaJacket.jpg', 79.99),
-('GH', 'Geisha Hoodie', 'img/products/geishaHoodie.jpg', 49.99),
-('AH', 'Adidas Hoodie', 'img/products/adidasHoodie.jpg', 59.99),
-('AMT', 'All Might T-Shirt', 'img/products/allMightShirt.jpg', 99.99),
-('CH', 'Champion Hoodie', 'img/products/championHoodie.jpg', 45.99),
-('CS', 'Champion Shirt', 'img/products/championShirt.jpg', 89.99),
-('NH', 'Nike Hoodie', 'img/products/nikeHoodie.jpg', 49.99);
+CREATE TABLE `admin` (
+  `adminID` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(10) NOT NULL,
+  `created_at` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`adminID`, `username`, `password`, `created_at`) VALUES
+(1, 'dito', '12345', '2024-06-05 12:24:38');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contactus`
+--
+
+CREATE TABLE `contactus` (
+  `nama` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `phone` varchar(50) NOT NULL,
+  `massage` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `contactus`
+--
+
+INSERT INTO `contactus` (`nama`, `email`, `phone`, `massage`) VALUES
+('raul', 'dtho2912@gmail.com', '085748083217', 'Permisi apa kabar'),
+('Fahros', 'fahros@gmail.com', '085748083217', 'PERMISI');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customeraccounts`
+--
+
+CREATE TABLE `customeraccounts` (
+  `CustomerID` int(11) NOT NULL,
+  `Email` varchar(50) DEFAULT NULL,
+  `Password` varchar(8) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `customeraccounts`
+--
+
+INSERT INTO `customeraccounts` (`CustomerID`, `Email`, `Password`) VALUES
+(1, 'khoirulansori407@gmail.com', '123'),
+(2, 'dtho2912@gmail.com', '123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `CustomerID` int(11) NOT NULL,
+  `OrderID` int(11) NOT NULL,
+  `OrderStatus` varchar(14) DEFAULT NULL,
+  `OrderDate` date DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `productorders`
+--
+
+CREATE TABLE `productorders` (
+  `OrderID` int(11) NOT NULL,
+  `ItemID` int(12) NOT NULL,
+  `Quanity` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `ItemID` int(12) NOT NULL,
+  `Code` varchar(20) NOT NULL,
+  `Name` varchar(30) DEFAULT NULL,
+  `Image` text NOT NULL,
+  `Price` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`ItemID`, `Code`, `Name`, `Image`, `Price`) VALUES
+(1, 'LMT', 'Lem Colek Inikol Takol', 'img/products/Lem Colek Inikol Takol.jpg', 1750),
+(2, 'LMC', 'Lem China', 'img/products/Lem China.jpg', 5000),
+(3, 'SP', 'Spidol Warna POSCA', 'img/products/Spidol Warna POSCA.jpg', 33000),
+(4, 'BPK', 'Bolpoint Kenko HI-TECH-H', 'img/products/Bolpoint Kenko HI-TECH-H.jpg', 20000),
+(5, 'PJK', 'Pensil Joyko kayu 2b', 'img/products/pensil joyko kayu 2b.jpg', 20000),
+(6, 'HJ', 'HIghlighter Joyko', 'img/products/HIghlighter Joyko.jpg', 23000),
+(7, 'DT', 'Double tape', 'img/products/double tape.jpg', 11000),
+(8, 'SK', 'Stapler Kecil', 'img/products/Stapler Kecil.jpg', 5000),
+(9, 'KRY', 'Krayon Putar 12 W', 'img/products/Krayon Putar 12 W.jpg', 25000);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`adminID`);
+
+--
+-- Indexes for table `contactus`
+--
+ALTER TABLE `contactus`
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `customeraccounts`
+--
+ALTER TABLE `customeraccounts`
+  ADD PRIMARY KEY (`CustomerID`),
+  ADD UNIQUE KEY `Email` (`Email`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`OrderID`),
+  ADD KEY `CustomerID` (`CustomerID`);
+
+--
+-- Indexes for table `productorders`
+--
+ALTER TABLE `productorders`
+  ADD KEY `OrderID` (`OrderID`),
+  ADD KEY `ItemID` (`ItemID`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`ItemID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `adminID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `customeraccounts`
+--
+ALTER TABLE `customeraccounts`
+  MODIFY `CustomerID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `OrderID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=666319909;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `ItemID` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`CustomerID`) REFERENCES `customeraccounts` (`CustomerID`);
+
+--
+-- Constraints for table `productorders`
+--
+ALTER TABLE `productorders`
+  ADD CONSTRAINT `productorders_ibfk_1` FOREIGN KEY (`OrderID`) REFERENCES `orders` (`OrderID`),
+  ADD CONSTRAINT `productorders_ibfk_2` FOREIGN KEY (`ItemID`) REFERENCES `products` (`ItemID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
